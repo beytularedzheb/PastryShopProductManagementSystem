6c5edcca8da64ff444d31059c9c83f118b168ef8
Beytula Redzheb <beytularedzheb@gmail.com>
2016-09-23 12:24:44 +0300

    Initial commit

---
+++ PastryShopProductManagementSystem/MainFrame.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace PastryShopProductManagementSystem
+{
+    partial class MainFrame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.documentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputDocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.outputDocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.productToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addProductToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.allProductsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unavailableProductsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.expiredProductsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dessertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addDessertToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.allDessertsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.providersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addProviderToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tlpMainFrame = new System.Windows.Forms.TableLayoutPanel();
+            this.btnNewInputDoc = new System.Windows.Forms.Button();
+            this.btnNewOutputDoc = new System.Windows.Forms.Button();
+            this.menuStripMain.SuspendLayout();
+            this.tlpMainFrame.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.documentsToolStripMenuItem,
+            this.dessertToolStripMenuItem,
+            this.productToolStripMenuItem,
+            this.providersToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(628, 24);
+            this.menuStripMain.TabIndex = 0;
+            // 
+            // documentsToolStripMenuItem
+            // 
+            this.documentsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inputDocToolStripMenuItem,
+            this.outputDocToolStripMenuItem});
+            this.documentsToolStripMenuItem.Name = "documentsToolStripMenuItem";
+            this.documentsToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.documentsToolStripMenuItem.Text = "Документи";
+            // 
+            // inputDocToolStripMenuItem
+            // 
+            this.inputDocToolStripMenuItem.Name = "inputDocToolStripMenuItem";
+            this.inputDocToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.inputDocToolStripMenuItem.Text = "Входящ контрол";
+            // 
+            // outputDocToolStripMenuItem
+            // 
+            this.outputDocToolStripMenuItem.Name = "outputDocToolStripMenuItem";
+            this.outputDocToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.outputDocToolStripMenuItem.Text = "Вложени материали";
+            // 
+            // productToolStripMenuItem
+            // 
+            this.productToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addProductToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.allProductsToolStripMenuItem,
+            this.unavailableProductsToolStripMenuItem,
+            this.expiredProductsToolStripMenuItem});
+            this.productToolStripMenuItem.Name = "productToolStripMenuItem";
+            this.productToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.productToolStripMenuItem.Text = "Продукти";
+            // 
+            // addProductToolStripMenuItem
+            // 
+            this.addProductToolStripMenuItem.Name = "addProductToolStripMenuItem";
+            this.addProductToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.addProductToolStripMenuItem.Text = "Добавяне";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(216, 6);
+            // 
+            // allProductsToolStripMenuItem
+            // 
+            this.allProductsToolStripMenuItem.Name = "allProductsToolStripMenuItem";
+            this.allProductsToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.allProductsToolStripMenuItem.Text = "Всички продукти";
+            // 
+            // unavailableProductsToolStripMenuItem
+            // 
+            this.unavailableProductsToolStripMenuItem.Name = "unavailableProductsToolStripMenuItem";
+            this.unavailableProductsToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.unavailableProductsToolStripMenuItem.Text = "Изчерпани";
+            // 
+            // expiredProductsToolStripMenuItem
+            // 
+            this.expiredProductsToolStripMenuItem.Name = "expiredProductsToolStripMenuItem";
+            this.expiredProductsToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.expiredProductsToolStripMenuItem.Text = "С изтекъл срок на госност";
+            // 
+            // dessertToolStripMenuItem
+            // 
+            this.dessertToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addDessertToolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.allDessertsToolStripMenuItem});
+            this.dessertToolStripMenuItem.Name = "dessertToolStripMenuItem";
+            this.dessertToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.dessertToolStripMenuItem.Text = "Изделия";
+            // 
+            // addDessertToolStripMenuItem1
+            // 
+            this.addDessertToolStripMenuItem1.Name = "addDessertToolStripMenuItem1";
+            this.addDessertToolStripMenuItem1.Size = new System.Drawing.Size(161, 22);
+            this.addDessertToolStripMenuItem1.Text = "Добавяне";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(158, 6);
+            // 
+            // allDessertsToolStripMenuItem
+            // 
+            this.allDessertsToolStripMenuItem.Name = "allDessertsToolStripMenuItem";
+            this.allDessertsToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.allDessertsToolStripMenuItem.Text = "Всички изделия";
+            // 
+            // providersToolStripMenuItem
+            // 
+            this.providersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addProviderToolStripMenuItem2});
+            this.providersToolStripMenuItem.Name = "providersToolStripMenuItem";
+            this.providersToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.providersToolStripMenuItem.Text = "Доставчици";
+            // 
+            // addProviderToolStripMenuItem2
+            // 
+            this.addProviderToolStripMenuItem2.Name = "addProviderToolStripMenuItem2";
+            this.addProviderToolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.addProviderToolStripMenuItem2.Text = "Добавяне";
+            // 
+            // tlpMainFrame
+            // 
+            this.tlpMainFrame.ColumnCount = 2;
+            this.tlpMainFrame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMainFrame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMainFrame.Controls.Add(this.btnNewInputDoc, 0, 0);
+            this.tlpMainFrame.Controls.Add(this.btnNewOutputDoc, 1, 0);
+            this.tlpMainFrame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMainFrame.Location = new System.Drawing.Point(0, 24);
+            this.tlpMainFrame.Name = "tlpMainFrame";
+            this.tlpMainFrame.RowCount = 1;
+            this.tlpMainFrame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMainFrame.Size = new System.Drawing.Size(628, 278);
+            this.tlpMainFrame.TabIndex = 1;
+            // 
+            // btnNewInputDoc
+            // 
+            this.btnNewInputDoc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNewInputDoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnNewInputDoc.Location = new System.Drawing.Point(3, 99);
+            this.btnNewInputDoc.Name = "btnNewInputDoc";
+            this.btnNewInputDoc.Size = new System.Drawing.Size(308, 80);
+            this.btnNewInputDoc.TabIndex = 0;
+            this.btnNewInputDoc.Text = "Нов документ за входящ контрол";
+            this.btnNewInputDoc.UseVisualStyleBackColor = true;
+            // 
+            // btnNewOutputDoc
+            // 
+            this.btnNewOutputDoc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNewOutputDoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnNewOutputDoc.Location = new System.Drawing.Point(317, 99);
+            this.btnNewOutputDoc.Name = "btnNewOutputDoc";
+            this.btnNewOutputDoc.Size = new System.Drawing.Size(308, 80);
+            this.btnNewOutputDoc.TabIndex = 1;
+            this.btnNewOutputDoc.Text = "Нов документ за вложени материали";
+            this.btnNewOutputDoc.UseVisualStyleBackColor = true;
+            // 
+            // MainFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 302);
+            this.Controls.Add(this.tlpMainFrame);
+            this.Controls.Add(this.menuStripMain);
+            this.MainMenuStrip = this.menuStripMain;
+            this.Name = "MainFrame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pastry Shop - Product Management System";
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            this.tlpMainFrame.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem documentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inputDocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem outputDocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dessertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addDessertToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem allDessertsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem productToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addProductToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem allProductsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unavailableProductsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem expiredProductsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem providersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addProviderToolStripMenuItem2;
+        private System.Windows.Forms.TableLayoutPanel tlpMainFrame;
+        private System.Windows.Forms.Button btnNewInputDoc;
+        private System.Windows.Forms.Button btnNewOutputDoc;
+    }
+}
+
