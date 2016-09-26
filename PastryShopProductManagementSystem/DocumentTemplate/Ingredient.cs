098f61f9c432b34f0516f75ba3eaa71b3d933ac6
vkostadinov <vasil.dm.kostadinov@gmai.com>
2016-09-26 14:45:04 +0300

    Added Ingredient class

---
+++ PastryShopProductManagementSystem/DocumentTemplate/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PastryShopProductManagementSystem.DocumentTemplate
+{
+    public class Ingredient
+    {
+        public string BatchNumber { get; set; }
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
