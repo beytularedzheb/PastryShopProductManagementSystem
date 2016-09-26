c2721219389725d30256e96a55958af9870690cb
Beytula Redzheb <beytularedzheb@gmail.com>
2016-09-26 11:48:16 +0300

    Added Input Document Mapper, validation and button to delete document line

---
+++ PastryShopProductManagementSystem/Frames/Helpers/InputDocumentMapper.cs
@@ -0,0 +1,87 @@
+﻿using PastryShopProductManagementSystem.Data;
+using PastryShopProductManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PastryShopProductManagementSystem.Frames.Helpers
+{
+    class InputDocumentMapper
+    {
+        public static InputDocument ReadValues(DataGridView gridView)
+        {
+            if (gridView == null || gridView.RowCount - 1 <= 0)
+            {
+                return null;
+            }
+
+            InputDocument inputDocument = new InputDocument();
+            ICollection<InputDocumentLine> lines = new LinkedList<InputDocumentLine>();
+            inputDocument.Lines = lines;
+
+            using (var db = new PastryShopDbContext())
+            {
+                int rowCount = gridView.RowCount - 1;
+                for (int i = 0; i < rowCount; i++)
+                {
+                    var receivingDate = gridView.Rows[i].Cells["ReceivingDate"].Value;
+                    var providerId = gridView.Rows[i].Cells["Provider"].Value;
+                    var productDetailId = gridView.Rows[i].Cells["Product"].Value;
+                    var receivedQuantity = gridView.Rows[i].Cells["ReceivedQuantity"].Value;
+                    var batchNumber = gridView.Rows[i].Cells["BatchNumber"].Value;
+                    var expiryDate = gridView.Rows[i].Cells["ExpiryDate"].Value;
+                    var storageCondition = gridView.Rows[i].Cells["StorageCondition"].Value;
+                    var vehicle = gridView.Rows[i].Cells["Vehicle"].Value;
+                    var document = gridView.Rows[i].Cells["Document"].Value;
+
+                    bool areAllRequiredFieldsFilled = batchNumber != null
+                        && receivingDate != null
+                        && productDetailId != null
+                        && expiryDate != null
+                        && receivedQuantity != null
+                        && providerId != null;
+
+                    if (areAllRequiredFieldsFilled)
+                    {
+                        InputDocumentLine line = new InputDocumentLine();
+                        line.InputDocument = inputDocument;
+                        line.ReceivingDate = Convert.ToDateTime(receivingDate.ToString());
+                        line.Provider = db.Providers.Find(providerId);
+                        line.ReceivedQuantity = Convert.ToDecimal(receivedQuantity.ToString());
+                        line.Vehicle = vehicle != null ? vehicle.ToString() : String.Empty;
+                        line.Document = document != null ? document.ToString() : String.Empty;
+
+                        var fetchedProductFromDB = (from p in db.Products
+                                                    where p.BatchNumber == batchNumber.ToString()
+                                                    select p).FirstOrDefault();
+                        if (fetchedProductFromDB == null)
+                        {
+                            Product product = new Product();
+                            product.AvailableQuantity = line.ReceivedQuantity;
+                            product.BatchNumber = batchNumber.ToString();
+                            product.ExpiryDate = Convert.ToDateTime(expiryDate);
+                            product.StorageCondition = storageCondition != null ? storageCondition.ToString() : String.Empty;
+                            product.ProductDetail = db.ProductDetails.Find(productDetailId);
+
+                            line.Product = product;
+                        }
+                        else
+                        {
+                            line.Product = fetchedProductFromDB;
+                            line.Product.AvailableQuantity += line.ReceivedQuantity;
+                        }
+
+                        lines.Add(line);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+
+            return inputDocument;
+        }
+    }
+}
--- PastryShopProductManagementSystem/Frames/InputDocumentCreationForm.Designer.cs
+++ PastryShopProductManagementSystem/Frames/InputDocumentCreationForm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnPanel = new System.Windows.Forms.Panel();
             this.dgwInputDocument = new System.Windows.Forms.DataGridView();
+            this.dataGridViewCalendarColumn1 = new PastryShopProductManagementSystem.CustomControls.DataGridViewCalendarColumn();
+            this.dataGridViewNumericUpDownColumn1 = new PastryShopProductManagementSystem.CustomControls.DataGridViewNumericUpDownColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewCalendarColumn2 = new PastryShopProductManagementSystem.CustomControls.DataGridViewCalendarColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ReceivingDate = new PastryShopProductManagementSystem.CustomControls.DataGridViewCalendarColumn();
             this.Provider = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.Product = new System.Windows.Forms.DataGridViewComboBoxColumn();
@@ -41,42 +48,43 @@
             this.StorageCondition = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Vehicle = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Document = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.panel1.SuspendLayout();
+            this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.btnPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgwInputDocument)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnSave
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(343, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(140, 46);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Запиши";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.Location = new System.Drawing.Point(11, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(140, 46);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Запиши";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
-            // button2
+            // btnCancel
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(503, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(140, 46);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Отказ";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCancel.Location = new System.Drawing.Point(157, 12);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(140, 46);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Отказ";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // panel1
+            // btnPanel
             // 
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 453);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(984, 70);
-            this.panel1.TabIndex = 3;
+            this.btnPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.btnPanel.Controls.Add(this.btnSave);
+            this.btnPanel.Controls.Add(this.btnCancel);
+            this.btnPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnPanel.Location = new System.Drawing.Point(0, 451);
+            this.btnPanel.Name = "btnPanel";
+            this.btnPanel.Size = new System.Drawing.Size(984, 70);
+            this.btnPanel.TabIndex = 3;
             // 
             // dgwInputDocument
             // 
@@ -92,7 +100,8 @@
             this.ExpiryDate,
             this.StorageCondition,
             this.Vehicle,
-            this.Document});
+            this.Document,
+            this.Delete});
             this.dgwInputDocument.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgwInputDocument.GridColor = System.Drawing.SystemColors.Control;
             this.dgwInputDocument.Location = new System.Drawing.Point(0, 0);
@@ -100,43 +109,111 @@
             this.dgwInputDocument.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.dgwInputDocument.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             this.dgwInputDocument.RowHeadersWidth = 25;
-            this.dgwInputDocument.Size = new System.Drawing.Size(984, 523);
+            this.dgwInputDocument.Size = new System.Drawing.Size(984, 521);
             this.dgwInputDocument.TabIndex = 0;
+            this.dgwInputDocument.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwInputDocument_CellContentClick);
+            // 
+            // dataGridViewCalendarColumn1
+            // 
+            this.dataGridViewCalendarColumn1.FillWeight = 66.01261F;
+            this.dataGridViewCalendarColumn1.HeaderText = "Дата на приемане";
+            this.dataGridViewCalendarColumn1.Name = "dataGridViewCalendarColumn1";
+            this.dataGridViewCalendarColumn1.Width = 116;
+            // 
+            // dataGridViewNumericUpDownColumn1
+            // 
+            this.dataGridViewNumericUpDownColumn1.FillWeight = 66.01261F;
+            this.dataGridViewNumericUpDownColumn1.HeaderText = "Количество";
+            this.dataGridViewNumericUpDownColumn1.Name = "dataGridViewNumericUpDownColumn1";
+            this.dataGridViewNumericUpDownColumn1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewNumericUpDownColumn1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.dataGridViewNumericUpDownColumn1.Width = 116;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.FillWeight = 66.01261F;
+            this.dataGridViewTextBoxColumn1.HeaderText = "Партида";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 116;
+            // 
+            // dataGridViewCalendarColumn2
+            // 
+            this.dataGridViewCalendarColumn2.FillWeight = 66.01261F;
+            this.dataGridViewCalendarColumn2.HeaderText = "Срок на годност";
+            this.dataGridViewCalendarColumn2.Name = "dataGridViewCalendarColumn2";
+            this.dataGridViewCalendarColumn2.Width = 117;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.FillWeight = 66.01261F;
+            this.dataGridViewTextBoxColumn2.HeaderText = "Условия на съхранение";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewTextBoxColumn2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.dataGridViewTextBoxColumn2.Width = 116;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.FillWeight = 66.01261F;
+            this.dataGridViewTextBoxColumn3.HeaderText = "Транспортно средство";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 116;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.FillWeight = 66.01261F;
+            this.dataGridViewTextBoxColumn4.HeaderText = "Придружаващ документ";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 116;
             // 
             // ReceivingDate
             // 
-            this.ReceivingDate.HeaderText = "Дата на приемане";
+            this.ReceivingDate.FillWeight = 66.01261F;
+            this.ReceivingDate.HeaderText = "*Дата на приемане";
             this.ReceivingDate.Name = "ReceivingDate";
             // 
             // Provider
             // 
-            this.Provider.HeaderText = "Доставчик";
+            this.Provider.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Provider.FillWeight = 66.01261F;
+            this.Provider.HeaderText = "*Доставчик";
             this.Provider.Name = "Provider";
+            this.Provider.Width = 80;
             // 
             // Product
             // 
-            this.Product.HeaderText = "Продукт";
+            this.Product.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Product.FillWeight = 66.01261F;
+            this.Product.HeaderText = "*Продукт";
             this.Product.Name = "Product";
+            this.Product.Width = 66;
             // 
             // ReceivedQuantity
             // 
-            this.ReceivedQuantity.HeaderText = "Количество";
+            this.ReceivedQuantity.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.ReceivedQuantity.FillWeight = 66.01261F;
+            this.ReceivedQuantity.HeaderText = "*Количество";
             this.ReceivedQuantity.Name = "ReceivedQuantity";
             this.ReceivedQuantity.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.ReceivedQuantity.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.ReceivedQuantity.Width = 106;
             // 
             // BatchNumber
             // 
-            this.BatchNumber.HeaderText = "Партида";
+            this.BatchNumber.FillWeight = 66.01261F;
+            this.BatchNumber.HeaderText = "*Партида";
             this.BatchNumber.Name = "BatchNumber";
             // 
             // ExpiryDate
             // 
-            this.ExpiryDate.HeaderText = "Срок на годност";
+            this.ExpiryDate.FillWeight = 66.01261F;
+            this.ExpiryDate.HeaderText = "*Срок на годност";
             this.ExpiryDate.Name = "ExpiryDate";
             // 
             // StorageCondition
             // 
+            this.StorageCondition.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.StorageCondition.FillWeight = 66.01261F;
             this.StorageCondition.HeaderText = "Условия на съхранение";
             this.StorageCondition.Name = "StorageCondition";
             this.StorageCondition.Resizable = System.Windows.Forms.DataGridViewTriState.True;
@@ -144,30 +221,43 @@
             // 
             // Vehicle
             // 
+            this.Vehicle.FillWeight = 66.01261F;
             this.Vehicle.HeaderText = "Транспортно средство";
             this.Vehicle.Name = "Vehicle";
             // 
             // Document
             // 
+            this.Document.FillWeight = 66.01261F;
             this.Document.HeaderText = "Придружаващ документ";
             this.Document.Name = "Document";
+            // 
+            // Delete
+            // 
+            this.Delete.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Delete.HeaderText = "";
+            this.Delete.MinimumWidth = 60;
+            this.Delete.Name = "Delete";
+            this.Delete.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Delete.Text = "Изтрий";
+            this.Delete.ToolTipText = "Изтрий";
+            this.Delete.UseColumnTextForButtonValue = true;
+            this.Delete.Width = 60;
             // 
             // InputDocumentCreationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(984, 523);
-            this.ControlBox = false;
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(984, 521);
+            this.Controls.Add(this.btnPanel);
             this.Controls.Add(this.dgwInputDocument);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MinimumSize = new System.Drawing.Size(1000, 560);
             this.Name = "InputDocumentCreationForm";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Създаване на нов документ за входящ контрол";
-            this.panel1.ResumeLayout(false);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.InputDocumentCreationForm_FormClosing);
+            this.btnPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgwInputDocument)).EndInit();
             this.ResumeLayout(false);
 
@@ -176,6 +266,16 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgwInputDocument;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Panel btnPanel;
+        private CustomControls.DataGridViewCalendarColumn dataGridViewCalendarColumn1;
+        private CustomControls.DataGridViewNumericUpDownColumn dataGridViewNumericUpDownColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private CustomControls.DataGridViewCalendarColumn dataGridViewCalendarColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
         private CustomControls.DataGridViewCalendarColumn ReceivingDate;
         private System.Windows.Forms.DataGridViewComboBoxColumn Provider;
         private System.Windows.Forms.DataGridViewComboBoxColumn Product;
@@ -185,8 +285,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn StorageCondition;
         private System.Windows.Forms.DataGridViewTextBoxColumn Vehicle;
         private System.Windows.Forms.DataGridViewTextBoxColumn Document;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewButtonColumn Delete;
     }
 }
